598c9da588b2540f8f748d8781bb088d5ae0af94
neel valani <neel@example.com>
2023-06-21 12:24:52 +0530

    Created variable 'runningQuestionIndex' to store the current question index. With help of these variable we render the mcq question on page. To check the mcq answer is right or wrong we have created function 'checKAnswer()'.
    Function:
    renderQuestion() - It render the mcq , when answer is provided
    scoreSheet() - It visible div when mcq is completed or time is completed.
    redirectTOHighscorePage() - To redirect to highscore page when click on
    submit. (Not working properly, it is redirecting to first mcq page)
    checkAnswer() - When click on any option the function will be called and
    give output as incorrect or correct.
    showdiv() - It visible the horizontal line and answer container.
    unshowdiv() - It hide the horizontal line and answer container.
    
    Link to style.css, mcq.css and mcq.js.
    ./assets/css/style.css -> for styling nav bar
    ./assets/css/mcq.css -> for styling mcq.html page
    assets/mcq.js -> for making mcq.html page responsive
    
    In header section we have created nav bar with two childs 'Highscore'
    and 'Time'. And in main section mcq container is created with
    dynamically rendering.

--- assets/mcq.js
+++ assets/mcq.js
@@ -1,3 +1,5 @@
+var runningQuestionIndex = 0;
+
 const questions = [
     {
       questionText: "Commonly used data types DO NOT include:",
@@ -38,3 +40,235 @@ const questions = [
       answer: "1. break",
     },
   ];
+
+let q = questions;
+
+
+//To load page when redirect on mcq.html page
+let load = document.getElementById("load");
+document.addEventListener('DOMContentLoaded',renderQuestion());
+
+
+//Using variable position it render the question.
+function renderQuestion() {
+    let q = questions;
+    document.getElementById("question_container").innerHTML = q[runningQuestionIndex].questionText;
+    document.getElementById("option1_container").innerHTML = q[runningQuestionIndex].options[0];
+    document.getElementById("option2_container").innerHTML = q[runningQuestionIndex].options[1];
+    document.getElementById("option3_container").innerHTML = q[runningQuestionIndex].options[2];
+    document.getElementById("option4_container").innerHTML = q[runningQuestionIndex].options[3];
+}
+
+
+//Dispaly score when mcq is completed.
+function scoresheet() {
+    const div=document.getElementById("quiz");
+    div.innerHTML = "<h1 id='alldone'>All Done!</h1> <h2 id='score'>Your final score is x.<h2> <form> <label id='ei'>Enter initials: </label> <input type='text'/><input type='submit' value='Submit' id='submit' onclick='redirectToHighscorePage()'/></form>"
+};
+
+
+//Redirect to Highscore page but not working properly its redirecting tofront page of mcq.html.
+function redirectToHighscorePage() {
+    console.log("hii");
+    location.redirect('highscore.html');
+}
+
+
+//when click on option it check the Answer and give output as Correct and Incorrect.
+function checkAnswer(e) {
+    if (runningQuestionIndex==0) {
+        if (e.target.firstChild.data == q[runningQuestionIndex].answer) {
+
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Correct!";
+            };
+            showdiv();
+
+            function unshowdiv() {
+                console.log("hii");
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex =+ 1;
+                renderQuestion();
+            };
+            
+            unshowdiv();
+            setTimeout(unshowdiv, 1000);
+
+        }
+        else {
+            
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Incorrect!";
+            };
+            showdiv();
+
+            function unshowdiv() {
+                console.log("hii");
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex =+ 1;
+                renderQuestion();
+            };
+            setTimeout(unshowdiv, 1000);
+        }
+    }
+    else if (runningQuestionIndex == 1) {
+        if (e.target.firstChild.data == q[runningQuestionIndex].answer) {
+            
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Correct!";
+            };
+            showdiv();
+            
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;;
+                renderQuestion();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+        else {
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Incorrect!";
+            };
+            showdiv();
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;
+                renderQuestion();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+    }
+    else if(runningQuestionIndex == 2) {
+        if (e.target.firstChild.data == q[runningQuestionIndex].answer) {
+            
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Correct!";
+            };
+            showdiv();
+            
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;;
+                renderQuestion();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+        else {
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Incorrect!";
+            };
+            showdiv();
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;
+                renderQuestion();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+    }
+    else if (runningQuestionIndex == 3) {
+        if (e.target.firstChild.data == q[runningQuestionIndex].answer) {
+            
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Correct!";
+            };
+            showdiv();
+            
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;;
+                renderQuestion();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+        else {
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Incorrect!";
+            };
+            showdiv();
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;
+                renderQuestion();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+    }
+    else if (runningQuestionIndex == 4) {
+        if (e.target.firstChild.data == q[runningQuestionIndex].answer) {
+            
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Correct!";
+            };
+            showdiv();
+            
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;
+                scoresheet();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+        else {
+            function showdiv() {
+                document.getElementById("horizontal_line").style.visibility = "visible";
+                document.getElementById("horizontal_line").innerHTML = "<hr></hr>" ;
+                document.getElementById("answer_container").style.visibility = "visible";
+                document.getElementById("answer_container").innerHTML = "Incorrect!";
+            };
+            showdiv();
+
+            function unshowdiv() {
+                document.getElementById("horizontal_line").style.visibility = "hidden";
+                document.getElementById("answer_container").style.visibility = "hidden";
+                runningQuestionIndex = runningQuestionIndex + 1;
+                scoresheet();
+            }
+            setTimeout(unshowdiv, 1000);
+        }
+    }
+}
