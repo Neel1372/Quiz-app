a8e3e50ee2f211583a1bb9dc0294d9ebfd842dd6
neel valani <neel@example.com>
2023-07-10 10:47:43 +0530

    sort highscore in decreasing order

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,14 +1,20 @@
-var scoreList= "<ol type='1'>";
-console.log(localStorage.key);
+var arrayList = [];
 for(var i =0;i< localStorage.length;i++){
-    scoreList += "<li>" +localStorage.key(i) + " - "+localStorage.getItem(localStorage.key(i))+"</li>";
+    arrayList.push({name: localStorage.key(i),score:localStorage.getItem(localStorage.key(i))})
+}
+arrayList.sort((a,b) => b.score - a.score);
+
+var scoreList= "<ol type='1'>";
+for(var i =0;i< arrayList.length;i++){
+    scoreList += "<li>" +arrayList[i].name + " - "+arrayList[i].score+"</li>";
 }
 scoreList += "</ol>"
+
+
 document.getElementById('scoreList').innerHTML= scoreList;
 
 function handleSubmit(){
     var name = document.getElementById('clear');
-    console.log("hii");
     localStorage.clear();
     location.reload();
 }
